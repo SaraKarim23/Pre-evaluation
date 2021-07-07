47beadebb26ab8fbddbba6a60baa9c420830366b
SaraKarim23 <sarabintekarim@gmail.com>
2021-07-07 16:39:14 +0600

    init

---
+++ helloWorld/src/helloWorld/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloWorld;
+
+/**
+ * @author rahman
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WOrking");
+
+	}
+
+}
